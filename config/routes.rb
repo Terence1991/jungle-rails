def4fb9222f39abad926fec4f0375adeb961b85a
Terence1991 <cplterencehawes@gmail.com>
2018-11-08 18:32:05 +0000

    user authentication implemented

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,17 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show] 
+    
+    # recources :profile, only: [:show, :update]
+    # recourse :session, only: [:create, :destroy]
   end
+  
+  get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+  
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
